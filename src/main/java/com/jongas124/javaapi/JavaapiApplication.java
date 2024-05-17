1b8a6499ae2865e6b11db78f5d7e3143805cc38e
jongas124 <kossos124@gmail.com>
2024-05-17 18:18:12 -0300

    Primeiro commit

---
+++ src/main/java/com/jongas124/javaapi/JavaapiApplication.java
@@ -0,0 +1,13 @@
+package com.jongas124.javaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jongas124/javaapi/JavaapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jongas124.javaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
